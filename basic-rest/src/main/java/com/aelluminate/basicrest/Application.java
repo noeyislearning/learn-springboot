d5e176c66f16adfb2359b4de22a6f711d2f6aa40
Noey <ignacio.francis@aelluminate.com>
2025-02-17 21:04:05 +0800

    feat: adds CommandLineRunner to initialize Run instance

--- basic-rest/src/main/java/com/aelluminate/basicrest/Application.java
+++ basic-rest/src/main/java/com/aelluminate/basicrest/Application.java
@@ -1,9 +1,17 @@
 package com.aelluminate.basicrest;
 
+import com.aelluminate.basicrest.run.Location;
+import com.aelluminate.basicrest.run.Run;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -13,6 +21,15 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         log.info("Application started.");
+    }
+
+    @Bean
+    CommandLineRunner runner() {
+        return args -> {
+            Run run = new Run(1, "Morning Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5,
+                    Location.OUTDOOR);
+            log.info("Run: {}", run);
+        };
     }
 
 }
--- basic-rest/src/main/java/com/aelluminate/basicrest/run/Run.java
+++ basic-rest/src/main/java/com/aelluminate/basicrest/run/Run.java
@@ -2,12 +2,6 @@ package com.aelluminate.basicrest.run;
 
 import java.time.LocalDateTime;
 
-public record Run(
-        Integer id,
-        String title,
-        LocalDateTime startedOn,
-        LocalDateTime completedOn,
-        Integer miles,
-        Location location
-) {
+public record Run(Integer id, String title, LocalDateTime startedOn, LocalDateTime completedOn, Integer miles,
+                Location location) {
 }
